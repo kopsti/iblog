7190266a089180248e6e68dd4987d01965401b2a
Timos Kopsidas <kopsidas.t@gmail.com>
2017-10-17 15:08:53 +0300

    Add about & home pages

--- src/iblog/urls.py
+++ src/iblog/urls.py
@@ -2,10 +2,13 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.conf.urls import include, url
 from django.contrib import admin
+from django.views.generic import TemplateView
 from registration.backends.hmac.views import RegistrationView
 from registration.forms import RegistrationFormUniqueEmail
 
 urlpatterns = [
+    url(r'^$', TemplateView.as_view(template_name="pages/index.html"), name='home'),
+    url(r'^about/$', TemplateView.as_view(template_name="pages/about.html"), name='about'),
     url(r'^admin/', admin.site.urls),
     url(r'^accounts/register/$',  RegistrationView.as_view(form_class=RegistrationFormUniqueEmail), name='auth_signup'),
     url(r'^accounts/', include('registration.backends.hmac.urls')),
